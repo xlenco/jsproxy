be7b1480f4f2a8bc76e2149ea1c83d719dad2999
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-03-05 19:31:43 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -13,15 +13,15 @@ jsproxy_config({
   // 节点配置
   node_map: {
     'mysite': {
-      label: 'done',
+      label: 'done节点',
       lines: {
-        'https://xlenco-jsproxy.deno.dev': 1,
+        'xlenco-jsproxy.deno.dev': 1,
       }
     },
-    'done': {
-      label: 'none',
+    'cf': {
+      label: 'cf节点',
       lines: {
-        '': 1,
+        'jp.nsso.eu.org': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
